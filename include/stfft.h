5e10457eca8258f48fe7b3663a981f79b93aff71
Jimmy Fitzpatrick <jcfitzpatrick12@gmail.com>
2025-09-10 21:33:13 +0100

    perf: Improve the stfft implementation

--- include/stfft.h
+++ include/stfft.h
@@ -2,55 +2,139 @@
 #define SPECTREL_STFFT_H
 
 #include <fftw3.h>
+#include <stdlib.h>
 
+/**
+ * @brief A discrete, complex-valued signal.
+ */
 typedef struct
 {
-    size_t num_samples;
-    fftw_complex *samples;
+    size_t num_samples;    /** The number of samples in the signal. */
+    fftw_complex *samples; /** The sample values. */
 } spectrel_signal_t;
 
-typedef struct
-{
-    size_t num_spectrums;
-    size_t num_samples_per_spectrum;
-    fftw_complex *samples;
-    double *times;
-    double *frequencies;
-} spectrel_spectrogram_t;
-
+/**
+ * A supported window type.
+ */
 typedef enum
 {
-    CONSTANT,
-    GAUSSIAN,
-    HANNING,
+    BOXCAR
 } spectrel_window_type_t;
 
-spectrel_signal_t make_window(const spectrel_window_type_t window_type,
-                              const size_t num_samples);
+/**
+ * @brief The spectrogram of a signal in units of DFT amplitude.
+ */
+typedef struct
+{
+    size_t num_spectrums; /** The number of spectrums in the spectrogram. */
+    size_t num_samples_per_spectrum; /** The number of samples in each
+                                         spectrum. */
+    fftw_complex *samples; /** The DFT amplitude of each spectral component,
+                               stored as a flat array. */
+    double *times;       /** The physical times assigned to each spectrum in the
+                             spectrogram. */
+    double *frequencies; /** The baseband frequencies assigned to each spectral
+                             component. */
+} spectrel_spectrogram_t;
 
-spectrel_signal_t make_cosine_signal(const size_t num_samples,
-                                     const double sample_rate,
-                                     const double frequency,
-                                     const double amplitude,
-                                     const double phase);
+/**
+ * @brief Print the number of samples in a signal, and the values of each
+ * sample.
+ * @param signal The signal to describe.
+ */
+void describe_signal(const spectrel_signal_t *signal);
 
+/**
+ * @brief Frees memory used by a signal.
+ * @param signal Pointer to the signal to free.
+ */
 void free_signal(spectrel_signal_t *signal);
 
-void free_spectrogram(spectrel_spectrogram_t *spectrum);
+/**
+ * @brief Create an empty, memory-aligned buffer for repeated in-place DFTs by
+ * FFTW.
+ * @param num_samples The size of the buffer.
+ * @return An empty signal.
+ */
+spectrel_signal_t *make_buffer(const size_t num_samples);
 
-spectrel_signal_t make_empty_signal(const size_t num_samples);
+/**
+ * @brief Sample a complex-valued cosine wave.
+ * @param num_samples The number of samples to take.
+ * @param sample_rate The rate at which to take samples.
+ * @param frequency The frequency of the cosine wave.
+ * @param amplitude The amplitude of the cosine wave.
+ * @param phase The initial phase of the cosine wave.
+ * @return The cosine wave signal.
+ */
+spectrel_signal_t *make_cosine_signal(const size_t num_samples,
+                                      const double sample_rate,
+                                      const double frequency,
+                                      const double amplitude,
+                                      const double phase);
 
-spectrel_spectrogram_t
-make_empty_spectrogram(const size_t num_spectrums,
-                       const size_t num_samples_per_spectrum);
+/**
+ * @brief Generate a real-valued constant signal.
+ * @param value The value of the real component of each sample in the signal.
+ * @return A signal where each sample's real part is set to the given value and
+ * the imaginary part is zero.
+ */
+spectrel_signal_t *make_constant_signal(const size_t num_samples,
+                                        const double value);
 
+/**
+ * @brief Create a window of a specified type and length.
+ * @param window_type The type of the window.
+ * @param num_samples The number of samples in the window.
+ * @return The window signal.
+ */
+spectrel_signal_t *make_window(const spectrel_window_type_t window_type,
+                               const size_t num_samples);
+
+/**
+ * @brief Plan a 1D, in-place DFT on a buffer.
+ *
+ * The buffer should be initialised with samples only after creating the plan.
+ *
+ * @param buffer An empty, uninitialised buffer on which the DFT will be
+ * performed.
+ * @return The FFTW plan.
+ */
 fftw_plan make_plan(spectrel_signal_t *buffer);
 
-spectrel_spectrogram_t stfft(fftw_plan p,
-                             spectrel_signal_t *buffer,
-                             const spectrel_signal_t *signal,
-                             const spectrel_signal_t *window,
-                             const size_t hop,
-                             const double sample_rate);
+/**
+ * @brief Compute the short-time discrete Fourier transform of the input signal
+ * using a real sliding window.
+ *
+ * The first window is centered at the start of the signal (index 0). The last
+ * window is the final one that fits entirely within the signal.
+ *
+ * @param p  A pre-planned FFTW plan for in-place transforms on the buffer.
+ * @param buffer An empty buffer, used for repeated in-place DFTs.
+ * @param window The window function, same length as the buffer.
+ * @param signal The input signal.
+ * @param window_hop The number of samples the window advances per frame.
+ * @param sample_rate The sample rate of the signal.
+ * @return A spectrogram containing the amplitude of each spectral component.
+ */
+spectrel_spectrogram_t *stfft(fftw_plan p,
+                              spectrel_signal_t *buffer,
+                              const spectrel_signal_t *window,
+                              const spectrel_signal_t *signal,
+                              const size_t window_hop,
+                              const double sample_rate);
 
-#endif // SPECTRAL_STFFT_H
+/**
+ * @brief Print properties of the spectrogram, and the values of each
+ * sample.
+ * @param signal The signal to describe.
+ */
+void describe_spectrogram(const spectrel_spectrogram_t *spectrogram);
+
+/**
+ * @brief Frees memory used by a spectrogram
+ * @param signal Pointer to the spectrogram to free.
+ */
+void free_spectrogram(spectrel_spectrogram_t *spectrogram);
+
+#endif
--- src/main.c
+++ src/main.c
@@ -1,9 +1,13 @@
+// Clean up and simplify the short-time fft implementation.
+// Return "big" structs by pointer, not by value.
+
 #include "spectrel.h"
 
-#include <math.h>
+#include <stdio.h>
 #include <stdlib.h>
+#include <string.h>
 
-void exit_with_failure(const char *msg)
+void exit_failure(const char *msg)
 {
     fprintf(stderr, "%s\n", msg);
     exit(EXIT_FAILURE);
@@ -11,47 +15,56 @@ void exit_with_failure(const char *msg)
 
 int main(int argc, char *argv[])
 {
-
-    // Define a cosine wave.
-    const size_t num_samples = 1024;
-    const double sample_rate = 256;
+    // Initialise the input signal.
+    const size_t num_samples = 32;
+    const double sample_rate = 8;
     const double frequency = 1;
-    const double phase = 0;
-    const double amplitude = 1;
-
-    // Define the window.
-    spectrel_window_type_t window_type = CONSTANT;
-    const size_t window_size = 128;
-    const size_t hop = 128;
-
-    // Make the window.
-    spectrel_signal_t window = make_window(window_type, window_size);
-
-    // Make the cosine wave.
-    spectrel_signal_t cosine_wave = make_cosine_signal(
+    const double amplitude = 1.0;
+    const double phase = 0.0;
+    spectrel_signal_t *signal = make_cosine_signal(
         num_samples, sample_rate, frequency, amplitude, phase);
 
-    // Make an empty buffer, where'll we'll keep performing in-place DFT's.
-    spectrel_signal_t buffer = make_empty_signal(window.num_samples);
-
-    // Check the memory allocations succeeded.
-    if (!window.samples || !cosine_wave.samples || !buffer.samples)
+    if (!signal)
     {
-        exit_with_failure("Memory allocation "
-                          "failed.");
+        exit_failure("Failed to allocate memory for the input signal.");
     }
 
-    // Make a reusable plan.
-    fftw_plan p = make_plan(&buffer);
+    // Initialise the window.
+    const size_t window_size = 8;
+    const size_t window_hop = 8;
+    const spectrel_window_type_t window_type = BOXCAR;
+    spectrel_signal_t *window = make_window(window_type, window_size);
 
-    // Perform the short-time FFT.
-    spectrel_spectrogram_t s =
-        stfft(p, &buffer, &cosine_wave, &window, hop, sample_rate);
+    if (!window)
+    {
+        exit_failure("Failed to allocate memory for the window.");
+    }
+
+    // Initialise the buffer (same size as the window.)
+    const size_t buffer_size = window_size;
+    spectrel_signal_t *buffer = make_buffer(buffer_size);
+
+    if (!buffer)
+    {
+        exit_failure("Failed to allocate memory for the buffer.");
+    }
+
+    // Plan an in-place DFT on the buffer.
+    fftw_plan p = make_plan(buffer);
+
+    // Compute the spectrogram.
+    spectrel_spectrogram_t *s =
+        stfft(p, buffer, window, signal, window_hop, sample_rate);
+
+    // Print it's properties.
+    describe_spectrogram(s);
 
     // Release memory.
+    free_signal(signal);
+    free_signal(window);
+    free_signal(buffer);
+    free_spectrogram(s);
     fftw_destroy_plan(p);
-    free_spectrogram(&s);
-    free_signal(&buffer);
-    free_signal(&cosine_wave);
-    free_signal(&window);
+
+    return EXIT_SUCCESS;
 }
