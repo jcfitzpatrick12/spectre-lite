3e8c33abdf94fed3cf06d9158ff675d32491b260
Jimmy Fitzpatrick <jcfitzpatrick12@gmail.com>
2025-09-11 21:32:50 +0100

    feat: Cleanup on fatal errors

--- src/main.c
+++ src/main.c
@@ -4,73 +4,104 @@
 #include <stdlib.h>
 #include <string.h>
 
-void exit_failure(const char *msg)
+int exit_failure(const char *msg)
 {
     fprintf(stderr, "%s\n", msg);
-    exit(EXIT_FAILURE);
+    return EXIT_FAILURE;
+}
+
+int exit_success()
+{
+    return EXIT_SUCCESS;
 }
 
 int main(int argc, char *argv[])
 {
+    // Initialise program status.
+    int status = EXIT_FAILURE;
+
+    // Initialise pointers.
+    spectrel_signal_t *signal = NULL;
+    spectrel_signal_t *window = NULL;
+    spectrel_signal_t *buffer = NULL;
+    fftw_plan p = NULL;
+    spectrel_spectrogram_t *s = NULL;
+
     // Initialise the input signal.
     const size_t num_samples = 32;
     const double sample_rate = 8;
     const double frequency = 1;
     const double amplitude = 1.0;
     const double phase = 0.0;
-    spectrel_signal_t *signal = make_cosine_signal(
+    signal = make_cosine_signal(
         num_samples, sample_rate, frequency, amplitude, phase);
 
     if (!signal)
-    {
-        exit_failure("Failed to allocate memory for the input signal.");
-    }
+        goto cleanup;
 
     // Initialise the window.
     const size_t window_size = 8;
     const size_t window_hop = 8;
     const spectrel_window_type_t window_type = BOXCAR;
-    spectrel_signal_t *window = make_window(window_type, window_size);
+    window = make_window(window_type, window_size);
 
     if (!window)
-    {
-        exit_failure("Failed to allocate memory for the window.");
-    }
+        goto cleanup;
 
     // Initialise the buffer (same size as the window.)
     const size_t buffer_size = window_size;
-    spectrel_signal_t *buffer = make_buffer(buffer_size);
+    buffer = make_buffer(buffer_size);
 
     if (!buffer)
-    {
-        exit_failure("Failed to allocate memory for the buffer.");
-    }
+        goto cleanup;
 
     // Plan an in-place DFT on the buffer.
-    fftw_plan p = make_plan(buffer);
+    p = make_plan(buffer);
+    if (!p)
+        goto cleanup;
 
     // Compute the spectrogram.
-    spectrel_spectrogram_t *s =
-        stfft(p, buffer, window, signal, window_hop, sample_rate);
+    s = stfft(p, buffer, window, signal, window_hop, sample_rate);
+    if (!s)
+        goto cleanup;
 
     // Print it's properties.
     describe_spectrogram(s);
 
-    // Release memory.
-    free_signal(signal);
-    signal = NULL;
+    // The program has succeeded.
+    status = EXIT_SUCCESS;
 
-    free_signal(window);
-    window = NULL;
+cleanup:
+    if (signal)
+    {
+        free_signal(signal);
+        signal = NULL;
+    }
 
-    free_signal(buffer);
-    buffer = NULL;
+    if (window)
+    {
+        free_signal(window);
+        window = NULL;
+    }
 
-    free_spectrogram(s);
-    s = NULL;
+    if (buffer)
+    {
+        free_signal(buffer);
+        buffer = NULL;
+    }
 
-    fftw_destroy_plan(p);
-    p = NULL;
+    if (p)
+    {
+        fftw_destroy_plan(p);
+        p = NULL;
+    }
 
-    return EXIT_SUCCESS;
+    if (s)
+    {
+        free_spectrogram(s);
+        s = NULL;
+    }
+
+    return (!status) ? exit_success()
+                     : exit_failure("An unexpected error has occured.");
 }
