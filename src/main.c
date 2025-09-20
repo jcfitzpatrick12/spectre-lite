b89962f7bbd29ec43a3465e34de902426b7fe88d
Jimmy Fitzpatrick <jcfitzpatrick12@gmail.com>
2025-09-20 16:32:01 +0100

    fix: Memory leak

--- src/main.c
+++ src/main.c
@@ -32,10 +32,10 @@ int main(int argc, char *argv[])
     // For now, hard-code them.
     const char *name = "hackrf";
     const double frequency = 95.8e6;
-    const double sample_rate = 2e6;
-    const double bandwidth = 2e6;
+    const double sample_rate = 20e6;
+    const double bandwidth = 20e6;
     const double gain = 20;
-    const size_t buffer_size = 1e5;
+    const size_t buffer_size = 1e6;
     const size_t window_size = 1024;
     const size_t window_hop = 512;
     spectrel_format_t format = SPECTREL_FORMAT_PGM;
@@ -45,9 +45,6 @@ int main(int argc, char *argv[])
         spectrel_make_receiver(name, frequency, sample_rate, bandwidth, gain);
 
     if (!receiver)
-        goto cleanup;
-
-    if (spectrel_activate_stream(receiver) != 0)
         goto cleanup;
 
     // Create a reusable buffer ready to read samples from the receiver into.
@@ -68,12 +65,25 @@ int main(int argc, char *argv[])
         window_size, SPECTREL_CONSTANT_SIGNAL, (void *)&window_params);
     if (!window)
         goto cleanup;
+        
+    // Set up the directory to write data produced at runtime.
+    dir = spectrel_get_dir();
+    if (spectrel_make_dir(dir) != 0)
+        goto cleanup;
 
     // Stream samples into the buffer, then apply the short-time DFT.
     // TODO: Make the duration of capture configurable, keeping track
     // of elapsed time via sample counting. For now, it's hard-coded.
-    for (size_t n = 0; n < 3; n++)
+    if (spectrel_activate_stream(receiver) != 0)
+        goto cleanup;
+
+    for (size_t n = 0; n < 20; n++)
     {
+        if (spectrogram)
+        {
+            spectrel_free_spectrogram(spectrogram);
+            spectrogram = NULL;
+        }
         if (spectrel_read_stream(receiver, buffer) != 0)
             goto cleanup;
         spectrogram =
@@ -82,41 +92,31 @@ int main(int argc, char *argv[])
             goto cleanup;
     }
 
-    // Write the latest spectrogram to file in the PGM format.
-    dir = spectrel_get_dir();
-    if (spectrel_make_dir(dir) != 0)
-        goto cleanup;
-
+    // Write the latest spectrogram to file in the PGM file format.
     file_path = spectrel_join(dir, "img.pgm");
     if (!file_path)
         goto cleanup;
-
     spectrel_write_spectrogram(spectrogram, file_path, format);
 
     status = SPECTREL_SUCCESS;
 
 cleanup:
-    if (dir)
-    {
-        free(dir);
-        dir = NULL;
-    }
     if (file_path)
     {
         free(file_path);
         file_path = NULL;
     }
-    spectrel_deactivate_stream(receiver);
+    if (dir)
+    {
+        free(dir);
+        dir = NULL;
+    }
     if (spectrogram)
     {
         spectrel_free_spectrogram(spectrogram);
         spectrogram = NULL;
     }
-    if (buffer)
-    {
-        spectrel_free_signal(buffer);
-        buffer = NULL;
-    }
+    spectrel_deactivate_stream(receiver);
     if (window)
     {
         spectrel_free_signal(window);
@@ -126,6 +126,11 @@ cleanup:
     {
         spectrel_free_plan(plan);
         plan = NULL;
+    }
+    if (buffer)
+    {
+        spectrel_free_signal(buffer);
+        buffer = NULL;
     }
     if (receiver)
     {
